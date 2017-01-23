81a30b3bef4fdaff7373ede29351ac5606e8831f
Kelli Parris <kelli@hsdesignhouse.com>
2017-01-23 10:58:59 -0600

    changed prod.rb for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,14 +87,14 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: "ballast-systems.herokuapp.com"
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: Rails.application.secrets.email_provider_username,
     password: Rails.application.secrets.email_provider_password
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => "ballast-systems.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
